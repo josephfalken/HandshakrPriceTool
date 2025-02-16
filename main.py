81dc93dfaf4d3ac0244165eb32cb0babfd59b6c3
josephfalken <josephfalken@me.com>
2025-02-16 15:34:08 -0500

    added requirements.txt for package dependencies

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ def hello():
     soup = BeautifulSoup(r.text)
     navLinks = soup.find_all("a", class_ = "gb1")
     return str(navLinks)
+
+#each route is an api call to my microservice
+@app.route('/getprices/<item>')
+def getprices (item):
+    ebayURL = f'https://www.ebay.com/sch/i.html?_nkw={item}&LH_Complete=1'
+    r = requests.get(ebayURL)
+    soup = BeautifulSoup(r.text)
+    prices = soup.find_all("span", class_="s-item__price")
+    return str (prices)
